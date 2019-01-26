f3bf7e33aa903188f30ca3fe3ea8d4b11e275c31
Nicholas Lurski <nlurski@gmail.com>
2019-01-26 15:45:22 -0500

    i think camera works for android, but not for ios. following this guide: http://xforms-kickstarter.com/#camera

---
+++ LipDemo.Android/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Provider;
+using Android.Net;
+using Java.IO;
+
+namespace LipDemo.Droid
+
+{
+    [Activity(Label = "LipDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        static readonly File file = new File(Android.OS.Environment.GetExternalStoragePublicDirectory(
+                                Android.OS.Environment.DirectoryPictures), "tmp.jpg");
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.SetVmPolicy(builder.Build());
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            (Xamarin.Forms.Application.Current as App).ShouldTakePicture += () => {
+                var intent = new Intent(MediaStore.ActionImageCapture);
+                intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(file));
+                StartActivityForResult(intent, 0);
+            };
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            (Xamarin.Forms.Application.Current as App).ShowImage(file.Path);
+            base.OnActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
---
+++ LipDemo/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace LipDemo
+{
+    public partial class App : Application
+    {
+        readonly Image image = new Image();
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                new Button {
+                    Text = "Take a picture!",
+                    Command = new Command(o => ShouldTakePicture()),
+                },
+                image,
+            },
+                },
+            };
+        }
+        public void ShowImage(string filepath)
+        {
+            image.Source = ImageSource.FromFile(filepath);
+        }
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        public event Action ShouldTakePicture = () => { };
+    }
+}
